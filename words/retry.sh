d2e2c81e9e34458a008540d5db150aaba1827ab4
Muhammed Haque <m.haque@livelinktechnology.net>
2018-04-19 17:33:22 +0100

    Adding script to retry missing

---
+++ words/retry.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+replace_line(){
+    head -n$[ ${2} - 1 ] ${1}
+    echo ${3}
+    tail -n$[ `wc -l <${1}` - ${2} +1 ] ${1}
+}
+
+EMPTIES=`grep -n '^$' ${1} | sed 's/:.*//g'`
+
+for n in ${EMPTIES}
+do
+    WORD=`./translate.sh $(sed 's/\.txt$//g' <<< $(sed 's/^_//g' <<< ${1})) $(sed "${n}q;d" _all.txt)`
+    echo ${WORD}
+    replace_line ${1} ${n} ${WORD} #> ${1}
+done
