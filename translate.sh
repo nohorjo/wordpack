a10b1e1a41ef543b996387e617d6b579c91a6c9d
Muhammed Haque <nohorjo@gmail.com>
2018-04-19 09:19:31 +0100

    Update translate.sh

--- translate.sh
+++ translate.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# requires pup
+# go get github.com/ericchiang/pup
+
 curl -s https://dictionary.cambridge.org/dictionary/english-${1}/${2} | cat |\
     pup 'span.trans:first-of-type' text{} |\
     head -n3 | tail -n1 | sed 's/^ *//g' | tr ',' '\n' | tr '/' '\n' | head -n1
