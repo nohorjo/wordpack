1e6a61e32165bdd305406f4ea250883a88c7bb63
Muhammed Haque <m.haque@livelinktechnology.net>
2018-04-20 12:31:52 +0100

    #1 fixed retry

--- words/retry.sh
+++ words/retry.sh
@@ -13,4 +13,5 @@ do
     WORD=$(./translate.sh $(sed 's/\.txt$//g' <<< $(sed 's/^_//g' <<< ${1})) $(sed "${n}q;d" _all.txt))
     echo ${n} ${WORD}
     replace_line ${1} ${n} ${WORD} > ${1}.new
+    mv ${1}.new ${1}
 done
