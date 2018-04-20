bb2ec4021b7e8045b4c9d93141446cb42b2200b4
Muhammed Haque <nohorjo@gmail.com>
2018-04-20 22:51:54 +0100

    Added follow redirect

--- words/translate.sh
+++ words/translate.sh
@@ -4,6 +4,6 @@
 # go get github.com/ericchiang/pup
 # https://stedolan.github.io/jq/
 
-html=$(curl -s https://dictionary.cambridge.org/dictionary/english-${1}/$(tr -dc '[[:print:]]' <<< "${2}" | tr '[[:upper:]]' '[[:lower:]]'))
+html=$(curl -L -s https://dictionary.cambridge.org/dictionary/english-${1}/$(tr -dc '[[:print:]]' <<< "${2}" | tr '[[:upper:]]' '[[:lower:]]'))
 selected=$(echo ${html} | pup 'span.trans' json{})
 echo ${selected} | jq '.[] | .text' | head -n5 | sed 's/"//g' | sed 's/$/$/g' | xargs echo | sed 's/\$ /$/g'
