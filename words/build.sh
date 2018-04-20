ed1431a02be6e5a4afa1dd524774883b1fc3d4cb
Muhammed Haque <m.haque@livelinktechnology.net>
2018-04-20 12:19:03 +0100

    Fixed replace line

--- words/build.sh
+++ words/build.sh
@@ -5,7 +5,7 @@ FILE=_${1}.txt
 rm ${FILE} 2> /dev/null
 
 while read w; do
-    WORD=`./translate.sh ${1} ${w}`
+    WORD=$(./translate.sh ${1} ${w})
     echo ${WORD}
     echo ${WORD} >> ${FILE}
 done <_all.txt
