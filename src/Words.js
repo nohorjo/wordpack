eaf78dd5d5fa445a6e31cbf3de9db9993b6eeb0b
Muhammed Haque <nohorjo@gmail.com>
2018-04-21 21:51:06 +0100

    #2 Added wordlist caching

--- src/Words.js
+++ src/Words.js
@@ -1,6 +1,25 @@
+import { AsyncStorage } from 'react-native';
+
+let loadFromRemote = true;
 
 export const listLanguages = async () => {
-    return await makeRequest("words");
+    const [cached, remote] = (await Promise.all([
+        AsyncStorage.getItem("LANGUAGES"),
+        makeRequest("words")
+    ])).map(x => {
+        if (typeof x == "string") x = JSON.parse(x);
+        if (x) x.date = new Date(x.date).valueOf();
+        return x;
+    });
+
+    if (cached && cached.date == remote.date) {
+        loadFromRemote = false;
+        return cached.languages;
+    }
+
+    AsyncStorage.setItem("LANGUAGES", JSON.stringify(remote));
+
+    return remote.languages;
 };
 
 export const getWords = async language => {
