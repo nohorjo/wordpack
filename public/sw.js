117b900de8be0a1623665bb28354165c14931979
Muhammed Haque <m.haque@livelinktechnology.net>
2018-11-27 13:36:24 +0000

    fixed cache

--- public/sw.js
+++ public/sw.js
@@ -1,15 +1,13 @@
 self.addEventListener('fetch', event => {
-    caches.open('wordpack').then(cache => {
-            event.respondWith(
-                caches.open('mysite-dynamic')
-                    .then(cache => cache.match(event.request))
-                    .then(response => {
-                        const update = fetch(event.request).then(response => {
-                            cache.put(event.request, response.clone());
-                            return response;
-                        });
-                        return response || update;
-                    })
-            );
-    });
+    event.respondWith(
+        caches.open('wordpack')
+            .then(cache => cache.match(event.request)
+            .then(response => {
+                const update = fetch(event.request).then(response => {
+                    cache.put(event.request, response.clone());
+                    return response;
+                });
+                return response || update;
+            }))
+    );
 });
